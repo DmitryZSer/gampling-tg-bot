37510699891dd1785a3cf9f05142ae7cd913c2e7
DmitryZSer <cebotarenkodima@gmail.com>
2024-09-30 23:27:17 +0300

    done

--- bot.py
+++ bot.py
@@ -1,160 +1,80 @@
 import telebot
 from telebot import types
-from modules.registration import register_user
-from pages.tropicana import handle_tropicana_selection, handle_registration_tropicana, handle_back_registration_tropicana, handle_rules_tropicana, handle_back_rules_tropicana, handle_back_tropicana
-from pages.mines import handle_mines_selection, handle_registration, handle_back_registration, handle_rules_mines, handle_back_rules, handle_back_mines, handle_exit_selection
-from resources import url, token
+from resources import token, url
 from modules.lang_handler import _, set_language
+from modules.game_handler import handle_game_selection, handle_game_rules, handle_game_registration, handle_game_signal
 
-# Ваш токен
 bot = telebot.TeleBot(token)
-
-# Запуск бота
 def start_bot():
     bot.polling()
 
-# Приветственное сообщение
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
-    # Отправляем сообщение с выбором языка
-
-    markup = types.InlineKeyboardMarkup()
-    btn_russian = types.InlineKeyboardButton("Русский язык", callback_data="russian")
-    markup.row(btn_russian)
-    btn_english = types.InlineKeyboardButton("English language", callback_data="english")
-    markup.row(btn_english)
-    
-    bot.send_message(message.chat.id, _('welcome.choose_language'), reply_markup=markup)
-
-# Удаление предыдущего сообщения перед отправкой нового
 def delete_previous_message(call, bot):
     try:
         bot.delete_message(call.message.chat.id, call.message.message_id)
     except Exception as e:
         print(f"Ошибка при удалении сообщения: {e}")
 
-# Обработка выбора языка
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    markup = types.InlineKeyboardMarkup()
+    btn_russian = types.InlineKeyboardButton("Русский язык", callback_data="russian")
+    btn_english = types.InlineKeyboardButton("English language", callback_data="english")
+    markup.row(btn_russian, btn_english)
+    bot.send_message(message.chat.id, _('welcome.choose_language'), reply_markup=markup)
+
 @bot.callback_query_handler(func=lambda call: call.data in ["russian", "english"])
 def handle_language_selection(call):
-    delete_previous_message(call, bot)  # Удаляем предыдущее сообщение
+    delete_previous_message(call, bot)
+    lang = 'ru' if call.data == "russian" else 'en'
+    set_language(lang)
+    show_game_selection(call.message)
 
-    if call.data == "russian":
-        set_language('ru')
-        # Отправляем баннер с изображением и кнопками
-        photo_path = 'img/photo.jpg'  # Замените на путь к вашей картинке
-        caption_text = "Выберите игру:"
+def show_game_selection(message):
+    photo_path = 'img/photo.jpg'
+    caption_text = _('welcome.game_shoice')
+    markup = types.InlineKeyboardMarkup()
+    games = ["Mines", "Tropicana", "Crash", "Lucky Jet"]
+    for i in range(0, len(games), 2):
+        row = [types.InlineKeyboardButton(game, callback_data=game.lower().replace(" ", "_")) for game in games[i:i+2]]
+        markup.row(*row)
+    with open(photo_path, 'rb') as photo:
+        bot.send_photo(message.chat.id, photo, caption=caption_text, reply_markup=markup)
 
-        markup = types.InlineKeyboardMarkup()
-        btn1 = types.InlineKeyboardButton("Mines", callback_data="mines")
-        btn2 = types.InlineKeyboardButton("Tropicana", callback_data="tropicana")
-        markup.row(btn1, btn2)
-        btn3 = types.InlineKeyboardButton("Crash", callback_data="crash")
-        btn4 = types.InlineKeyboardButton("Lucky Jet", callback_data="lucky_jet")
-        markup.row(btn3, btn4)
+@bot.callback_query_handler(func=lambda call: call.data in ["mines", "tropicana", "crash", "lucky_jet"])
+def game_selection(call):
+    delete_previous_message(call, bot)
+    handle_game_selection(call, bot, call.data)
 
-        # Отправка изображения с текстом и кнопками
-        with open(photo_path, 'rb') as photo:
-            bot.send_photo(call.message.chat.id, photo, caption=caption_text, reply_markup=markup)
-    elif call.data == "english":
-        set_language('en')
+@bot.callback_query_handler(func=lambda call: call.data.startswith("rules_"))
+def rules_selection(call):
+    delete_previous_message(call, bot)
+    game_name = call.data.split("_")[1]
+    handle_game_rules(call, bot, game_name)
 
-# Обработка выбора игры mines
-@bot.callback_query_handler(func=lambda call: call.data == "mines")
-def mines_selection(call):
-    delete_previous_message(call, bot)  # Удаляем предыдущее сообщение
-    handle_mines_selection(call, bot)  # Передаем объект bot
-
-# Обработка выбора игры tropicana
-@bot.callback_query_handler(func=lambda call: call.data == "tropicana")
-def tropicana_selection(call):
-    delete_previous_message(call, bot)  # Удаляем предыдущее сообщение
-    bot.send_message(call.message.chat.id, _('tropicana.intro_test'))
-    #handle_tropicana_selection(call, bot)  # Передаем объект bot
-
-# Обработка выбора игры crash
-@bot.callback_query_handler(func=lambda call: call.data == "crash")
-def tropicana_selection(call):
-    delete_previous_message(call, bot)  # Удаляем предыдущее сообщение
-    bot.send_message(call.message.chat.id, _('crash.intro_test'))
-    #handle_crash_selection(call, bot)
-
-# Обработка выбора игры lucky_jet
-@bot.callback_query_handler(func=lambda call: call.data == "lucky_jet")
-def tropicana_selection(call):
-    delete_previous_message(call, bot)  # Удаляем предыдущее сообщение
-    bot.send_message(call.message.chat.id, _('lucky_jet.intro_test'))
-    #handle_lucky_jet_selection(call, bot)
-
-
-# Обработка выбора регистрации
-@bot.callback_query_handler(func=lambda call: call.data == "registration")
+@bot.callback_query_handler(func=lambda call: call.data.startswith("registration_"))
 def registration_selection(call):
-    delete_previous_message(call, bot)  # Удаляем предыдущее сообщение
-    handle_registration(call, bot, url)
+    delete_previous_message(call, bot)
+    game_name = call.data.split("_")[1]
+    handle_game_registration(call, bot, url, game_name)
 
-# Обработка выбора регистрации tropicana
-@bot.callback_query_handler(func=lambda call: call.data == "registration_tropicana")
-def registration_selection_tropicana(call):
-    delete_previous_message(call, bot)  # Удаляем предыдущее сообщение
-    handle_registration_tropicana(call, bot, url)
+@bot.callback_query_handler(func=lambda call: call.data.startswith("back_in_game_"))
+def registration_selection(call):
+    delete_previous_message(call, bot)
+    game_name = call.data.split("_")[-1]
+    handle_game_selection(call, bot, game_name)
 
-# Обработка кнопки "Назад" на экране регистрации
-@bot.callback_query_handler(func=lambda call: call.data == "back_registration")
-def back_registration_selection(call):
-    delete_previous_message(call, bot)  # Удаляем предыдущее сообщение
-    handle_back_registration(call, bot)
+@bot.callback_query_handler(func=lambda call: call.data.startswith("signal_"))
+def signal_selection(call):
+    delete_previous_message(call, bot)
+    game_name = call.data.split("_")[1]
+    handle_game_signal(call, bot, game_name)
 
-# Обработка кнопки "Назад" на экране регистрации
-@bot.callback_query_handler(func=lambda call: call.data == "back_registration_tropicana")
-def back_registration_selection_tropicana(call):
-    delete_previous_message(call, bot)  # Удаляем предыдущее сообщение
-    handle_back_registration_tropicana(call, bot)
-
-# Обработка выбора инструкции
-@bot.callback_query_handler(func=lambda call: call.data == "rules_mines")
-def rules_mines_selection(call):
-    delete_previous_message(call, bot)  # Удаляем предыдущее сообщение
-    handle_rules_mines(call, bot)
-
-# Обработка выбора инструкции
-@bot.callback_query_handler(func=lambda call: call.data == "rules_tropicana")
-def rules_tropicana_selection(call):
-    delete_previous_message(call, bot)  # Удаляем предыдущее сообщение
-    handle_rules_tropicana(call, bot)
-
-# Обработка кнопки "Назад" на экране инструкции
-@bot.callback_query_handler(func=lambda call: call.data == "exit_mines")
-def back_rules_selection(call):
-    delete_previous_message(call, bot)  # Удаляем предыдущее сообщение
-    handle_back_rules(call, bot)
-
-# Обработка кнопки "Назад" на экране инструкции
-@bot.callback_query_handler(func=lambda call: call.data == "exit_tropicana")
-def back_rules_selection_tropicana(call):
-    delete_previous_message(call, bot)  # Удаляем предыдущее сообщение
-    handle_back_rules_tropicana(call, bot)
-
-# Обработка кнопки "Выдать сигнал"
-@bot.callback_query_handler(func=lambda call: call.data == "back_mines")
-def back_mines_selection(call):
-    delete_previous_message(call, bot)  # Удаляем предыдущее сообщение
-    handle_back_mines(call, bot)
-
-# Обработка кнопки "Выдать сигнал"
-@bot.callback_query_handler(func=lambda call: call.data == "back_tropicana")
-def back_tropicana_selection(call):
-    delete_previous_message(call, bot)  # Удаляем предыдущее сообщение
-    handle_back_tropicana(call, bot)
-
-# Обработка кнопки "Выбор языка"
-@bot.callback_query_handler(func=lambda call: call.data == "start")
+@bot.callback_query_handler(func=lambda call: call.data == "main_menu")
 def handle_start_selection(call):
-    delete_previous_message(call, bot)  # Удаляем предыдущее сообщение
-    send_welcome(call.message)
+    delete_previous_message(call, bot)
+    show_game_selection(call.message)
 
-# Обработка текстовых сообщений
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
     bot.reply_to(message, _('menu.error_message'))
-    send_welcome(message)
-
+    show_game_selection(message)
---
+++ modules/game_handler.py
@@ -0,0 +1,58 @@
+from telebot import types
+from modules.registration import is_user_registered, register_user
+from modules.lang_handler import _
+
+from resources import url, webapp_url
+
+
+def handle_game_selection(call, bot, game_name):
+    photo_path = f'img/{game_name}.jpg'
+    caption_text = _(f'{game_name}.{game_name}_caption')
+
+    markup = types.InlineKeyboardMarkup()
+    btn_registration = types.InlineKeyboardButton(_("menu.registration"), callback_data=f"registration_{game_name}")
+    btn_instruction = types.InlineKeyboardButton(_("menu.instruction"), callback_data=f"rules_{game_name}")
+    markup.row(btn_registration, btn_instruction)
+    btn_signal = types.InlineKeyboardButton(_("menu.issue_a_signal"), callback_data=f"signal_{game_name}")
+    btn_language = types.InlineKeyboardButton(_("menu.back"), callback_data="main_menu")
+    markup.row(btn_signal, btn_language)
+
+    with open(photo_path, 'rb') as photo:
+        bot.send_photo(call.message.chat.id, photo, caption=caption_text, reply_markup=markup)
+
+
+def handle_game_rules(call, bot, game_name):
+    instruction_text = _(f'menu.instruction_text')
+    instruction_photo_path = f'img/inst.jpg'
+
+    markup = types.InlineKeyboardMarkup()
+    btn_back = types.InlineKeyboardButton(_('menu.back'), callback_data=f"back_in_game_{game_name}")
+    markup.row(btn_back)
+
+    with open(instruction_photo_path, 'rb') as photo:
+        bot.send_photo(call.message.chat.id, photo, caption=instruction_text, reply_markup=markup)
+
+
+def handle_game_registration(call, bot, url, game_name):
+    register_user(call.from_user.id)
+    registration_text = _('menu.registration_text')
+
+    markup = types.InlineKeyboardMarkup()
+    btn_register = types.InlineKeyboardButton(_('menu.register'), url=url)
+    markup.row(btn_register)
+    btn_back = types.InlineKeyboardButton(_('menu.back'), callback_data=f"back_in_game_{game_name}")
+    markup.row(btn_back)
+
+    bot.send_message(call.message.chat.id, registration_text, reply_markup=markup)
+
+
+def handle_game_signal(call, bot, game_name):
+    user_id = call.from_user.id
+    if is_user_registered(user_id):
+        markup = types.InlineKeyboardMarkup()
+        webapp_button = types.InlineKeyboardButton(_("menu.get_a_signal"), web_app=types.WebAppInfo(webapp_url))
+        markup.add(webapp_button)
+        bot.send_message(call.message.chat.id, _('menu.you_are_registered'), reply_markup=markup)
+    else:
+        bot.send_message(call.message.chat.id, _('menu.error_register'))
+        handle_game_registration(call, bot, url, game_name)
